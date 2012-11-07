a8a8fabbbb52da9600e34a9f7f408cfab2d64f54
Soesah <carlgiesberts@gmail.com>
2012-11-07 22:46:08 +0100

    Added node ClassLoader, the file to process the class Javascript files.

---
+++ nodeClassloader.js
@@ -0,0 +1,29 @@
+/*
+ *  Node Class Loader
+ */
+
+
+// register the package
+function Package(str)
+{
+  console.log(str);
+}
+
+// import other classes
+function Import()
+{
+  console.log(args);
+}
+
+// extends other classes
+function Extends()
+{
+  console.log(args);
+}
+
+// setup a class
+function Class () 
+{
+  console.log(args);
+}
+
