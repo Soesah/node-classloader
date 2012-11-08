3da4ce363360653abc3de85d059f9c401f4ada64
Soesah <carlgiesberts@gmail.com>
2012-11-08 11:16:30 +0100

    Added a classloader in php to oversee the process and return the final compiled script

---
+++ classloader.php
@@ -0,0 +1,88 @@
+<?php 
+
+
+Class ClassLoader
+{
+  function __construct($path, $package)
+  {
+    $this->tempfile = "filelist.js";
+    $this->path = $path;
+    $this->package = $package;
+
+    // get a file list
+    $this->getFileList();
+
+    // save file list
+    $this->saveFileList();
+
+    // get node to concatenate this
+    $this->nodeConcatenator();
+
+    // get node to compile it
+
+    // get node to minify it
+
+    // write it out
+
+    // clean up
+    $this->cleanup();
+
+  }
+
+  private function getFileList()
+  {
+    $this->sources = $this->glob_deep("*.js", $this->path);
+  }
+
+  private function saveFileList()
+  {
+    $filelist = $this->tempfile;
+    $fh = fopen($filelist, 'w') or die("can't open file");
+    fwrite($fh, "var filelist = [\n");
+    foreach ($this->sources as $file) 
+    {
+      fwrite($fh, "'".$file."'\n");
+      if($file != $this->sources[count($this->sources)-1])
+        fwrite($fh, ",");
+    }
+    fwrite($fh, "];\n");
+    fclose($fh);    
+  }
+
+  private function nodeConcatenator()
+  {
+    echo file_get_contents($this->tempfile);
+    // $val = "";
+    // passthru("node Concatentor.js 2>&1", $val);
+    // echo $val;
+  }
+
+  public function output()
+  {
+
+  }
+
+  private function glob_deep($pattern = '*', $path, $flags = 0)
+  {
+    $dirs = glob($path.'*', GLOB_MARK | GLOB_ONLYDIR | GLOB_NOSORT);
+    $files = glob($path.$pattern, $flags);
+
+    foreach($dirs as $dir)
+      $files = array_merge($files, $this->glob_deep($pattern, $dir, $flags));
+
+    return $files;
+  }
+
+  private function cleanup()
+  {
+    unlink($this->tempfile);
+  }
+}
+
+$package = (isset($_GET["package"]))?$_GET["package"]:"com";
+
+$cl = new ClassLoader("source\\", $package);
+
+$cl->output();
+
+?>
