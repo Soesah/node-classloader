823840cae45ccb822cbead3d4170f27c9d49f67e
Soesah <carlgiesberts@gmail.com>
2012-11-11 20:40:20 +0100

    Classloader parses concatenated source, and turns this into class objects

---
+++ Class.js
@@ -0,0 +1,66 @@
+
+
+var Class = (function(){
+  
+
+  var Class = function Class(namespaceURI)
+  { 
+    this.imports = [];
+    this.extend = null;
+    this.dependencies = [];
+    this.methods = [];
+    this.namespaceURI = namespaceURI;
+    this.flag = null;
+  } 
+
+  Class.prototype.Class = Class;
+
+  Class.prototype.setSingleton = function() 
+  {
+    this.singleton = true;
+  };
+  Class.prototype.setFlag = function(flag) 
+  {
+    this.flag = flag;
+  };
+
+  Class.prototype.setConstructor = function(name, f) 
+  {
+    this.constr = {name: name, method: f};
+  };
+
+  Class.prototype.addMethod = function(name, f) 
+  {
+    var method = {name: name, method: f};
+    if(this.flag)
+      method.flag = this.flag;
+    this.methods.push(method);
+    this.setFlag(null);
+  };
+
+  Class.prototype.addImport = function(className) 
+  {
+    this.imports.push(className);
+    this.addDependency(className);
+  };
+
+  Class.prototype.addExtend = function(className) 
+  {
+    this.extend = className;
+    this.addDependency(className);
+  };
+
+  Class.prototype.setResource = function(name, resource) 
+  {
+    this[name] = resource;
+  };
+
+  Class.prototype.addDependency = function(className) 
+  {
+    this.dependencies.push(className);
+  };
+
+  return Class;
+})();
+
+module.exports = Class;
--- Classloader.js
+++ Classloader.js
@@ -4,15 +4,23 @@
 
 var ClassLoader = (function(){
 
+  var c = require("./Class.js");
+  
   var ClassLoader = function ClassLoader()
   {
+    this.classList = new Array();
   };
 
   ClassLoader.prototype.ClassLoader = ClassLoader;
 
   // register the package
-  ClassLoader.prototype.Package =  function Package(namespaceURI)
+  ClassLoader.prototype.Package = function Package(namespaceURI)
   {
+    var cl = new c(namespaceURI);
+
+    this.classList.push(cl);
+    this.currentClass = cl;
+
     var namespaces = namespaceURI.split(".");
     var obj = this.namespaces;
 
@@ -32,38 +40,72 @@ var ClassLoader = (function(){
   // import other classes
   ClassLoader.prototype.Import = function Import()
   {
-    // console.log(arguments);
+    for (var i = 0; i < arguments.length; i++) 
+    {
+     var importClass = arguments[i];
+     this.currentClass.addImport(importClass);
+    };
   };
 
   // extends other classes
   ClassLoader.prototype.Extends = function Extends()
   {
-    // console.log(arguments);
+    for (var i = 0; i < arguments.length; i++) 
+    {
+     var extendClass = arguments[i];
+     this.currentClass.addExtend(extendClass);
+    };  
   };
 
   // setup a class
-  ClassLoader.prototype.Class = function Class () 
+  ClassLoader.prototype.Class = function Class() 
   {
-    // console.log(arguments);
+    for (var i = 0; i < arguments.length; i++) 
+    {
+      var arg = arguments[i];
+      var name = this.getName(arg);
+      if(typeof arg == 'function')
+      {
+        if(i == 0)
+          this.currentClass.setConstructor(name, arg);
+        else
+          this.currentClass.addMethod(name, arg);
+      }
+      else
+        this.currentClass.setFlag(arg);        
+    };  
   };
 
   ClassLoader.prototype.Singleton = function Singleton () 
   {
-    // console.log(arguments);
+    this.currentClass.setSingleton();
+    this.Class(arguments);
   };
 
-  ClassLoader.prototype.CSSResource = function CSSResource () 
+  ClassLoader.prototype.CSSResource = function CSSResource (rsc) 
   {
-    // console.log(arguments);
+    this.currentClass.setResource("CSSResource", rsc);
   };
 
-  ClassLoader.prototype.XMLResource = function XMLResource () 
+  ClassLoader.prototype.XMLResource = function XMLResource (rsc) 
   {
-    // console.log(arguments);
+    this.currentClass.setResource("XMLResource", rsc);
+  };
+
+  ClassLoader.prototype.getName = function getName(f) {
+    var str = f.toString();
+    var name = str.substring(str.indexOf(' ')+1, str.indexOf("("));
+    if(name == '' || name == ' ')
+      return name;
+    else
+      return name;
   };
 
   ClassLoader.prototype.namespaces = {};
-  ClassLoader.prototype.Static = true;
+  ClassLoader.prototype.Static = "static";
+  ClassLoader.prototype.Protected = "protected";
+  ClassLoader.prototype.Public = "public";
+  ClassLoader.prototype.Static = "static";
 
 
   return ClassLoader;
