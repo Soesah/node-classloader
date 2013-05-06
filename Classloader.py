53ba192626cfb71852592c5f5e3fc1889d7116f5
Soesah <carlgiesberts@gmail.com>
2013-05-06 17:33:42 +0200

    Added a path for the classloader to be found on, default ''

--- Classloader.py
+++ Classloader.py
@@ -5,16 +5,17 @@ import subprocess
 
 class Classloader():
   """Python implementation of server script to run the node classloader"""
-  def __init__(self, path, package):
+  def __init__(self, path, package, classloaderpath=""):
     self.path = path
     self.package = package
+    self.classloaderpath = classloaderpath
 
     self.nodepath = ""
     if sys.platform == "darwin": # Mac OS Apache can't find node without full path
       self.nodepath = "/usr/local/bin/"
 
   def nodeClassloader(self):
-    command = [self.nodepath+"node", "Classloader.js", self.path,self.package]
+    command = [self.nodepath+"node", self.classloaderpath + "Classloader.js", self.path,self.package]
     process = subprocess.Popen(command, stdout=subprocess.PIPE)
     script = process.stdout.read().decode("utf-8") 
     return script
