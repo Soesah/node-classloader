1613bed2c89de3fbd6a4bea569d63016a45b7e68
Soesah <carlgiesberts@gmail.com>
2013-05-06 17:41:14 +0200

    Update classloader to be usable from command line. Will always output javascript

--- Classloader.py
+++ Classloader.py
@@ -6,8 +6,12 @@ import subprocess
 class Classloader():
   """Python implementation of server script to run the node classloader"""
   def __init__(self, path, package, classloaderpath=""):
+    if package == None:
+      raise Exception("Classloader Error: no package found")
+
     self.path = path
     self.package = package
+  
     self.classloaderpath = classloaderpath
 
     self.nodepath = ""
@@ -21,14 +25,14 @@ class Classloader():
     return script
 
 if __name__ == "__main__":
-
   path = "../source"
   package = None
 
   if len(sys.argv) > 1:
     package = sys.argv[1];
 
-  if package == None:
-    print "console.error('Classloader Error: no package found');"
-  else:
-    print Classloader(path, package).nodeClassloader()
+  try:
+    classloader = Classloader(path, package)
+    print classloader.nodeClassloader()
+  except Exception, e:
+    print "console.log('"+e.message+"');"
